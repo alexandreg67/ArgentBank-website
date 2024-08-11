19d0098cfe28c851adc5db9fd0fa58375cbe544a
Graff Alexandre <alexgraff67@gmail.com>
2024-08-11 16:14:33 +0200

    ajout message d'erreur composant login

--- src/components/EditUserForm/index.jsx
+++ src/components/EditUserForm/index.jsx
@@ -14,7 +14,7 @@ const EditUserForm = ({ user, onCancel }) => {
 	};
 
 	return (
-		<div className="edit-user-form">
+		<section className="edit-content">
 			<h1>Edit user info</h1>
 			<form onSubmit={handleSubmit}>
 				<div className="input-wrapper">
@@ -43,7 +43,37 @@ const EditUserForm = ({ user, onCancel }) => {
 					</button>
 				</div>
 			</form>
-		</div>
+		</section>
+		// <div className="edit-user-form">
+		// 	<h1>Edit user info</h1>
+		// <form onSubmit={handleSubmit}>
+		// 	<div className="input-wrapper">
+		// 		<label htmlFor="username">User Name :</label>
+		// 		<input
+		// 			type="text"
+		// 			id="username"
+		// 			value={username}
+		// 			onChange={(e) => setUsername(e.target.value)}
+		// 		/>
+		// 	</div>
+		// 	<div className="input-wrapper">
+		// 		<label htmlFor="firstName">First Name :</label>
+		// 		<input type="text" id="firstName" value={user.firstName} disabled />
+		// 	</div>
+		// 	<div className="input-wrapper">
+		// 		<label htmlFor="lastName">Last Name :</label>
+		// 		<input type="text" id="lastName" value={user.lastName} disabled />
+		// 	</div>
+		// 	<div className="button-group">
+		// 		<button type="submit" className="update-button">
+		// 			Save
+		// 		</button>
+		// 		<button type="button" className="cancel-button" onClick={onCancel}>
+		// 			Cancel
+		// 		</button>
+		// 	</div>
+		// </form>
+		// </div>
 	);
 };
 
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,65 +1,92 @@
-import React from 'react';
-import './index.css';
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { loginUser, fetchUserProfile } from '../../features/user/userSlice';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, fetchUserProfile } from "../../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [localError, setLocalError] = useState({});
+	const [serverError, setServerError] = useState("");
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const userStatus = useSelector((state) => state.user.status);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const userStatus = useSelector((state) => state.user.status);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(loginUser({ email, password }));
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-  useEffect(() => {
-    if (userStatus === 'succeeded') {
-      dispatch(fetchUserProfile());
-      navigate('/user');
-    }
-  }, [userStatus, dispatch, navigate]);
+		let errors = {};
 
-  return ( 
-    <main className="main bg-dark">
-      <section className="sign-in-content">
-        <i className="fa fa-user-circle sign-in-icon"></i>
-        <h1>Sign In</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="input-wrapper">
-            <label htmlFor="email">Username</label>
-            <input 
-              type="email" 
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)} 
-            />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="password">Password</label>
-            <input 
-              type="password" id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)} 
-            />
-          </div>
-          <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
-            <label htmlFor="remember-me">Remember me</label>
-          </div>
+		if (!email) {
+			errors.email = "Email is required";
+		}
+		if (!password) {
+			errors.password = "Password is required";
+		}
 
-          <button type='submit' className="sign-in-button">Sign In</button>
-        </form>
-      </section>
+		setLocalError(errors);
 
-    </main>
-  );
-}
-  
+		if (Object.keys(errors).length === 0) {
+			dispatch(loginUser({ email, password }))
+				.unwrap()
+				.catch((e) => {
+					console.log("error", e);
+					setServerError(e.message);
+				});
+		}
+	};
+	useEffect(() => {
+		if (userStatus === "succeeded") {
+			dispatch(fetchUserProfile());
+			navigate("/user");
+		}
+	}, [userStatus, dispatch, navigate]);
+
+	return (
+		<main className="main bg-dark">
+			<section className="sign-in-content">
+				<i className="fa fa-user-circle sign-in-icon"></i>
+				<h1>Sign In</h1>
+				<form onSubmit={handleSubmit}>
+					<div className="input-wrapper">
+						<label htmlFor="email">Email</label>
+						<input
+							type="email"
+							id="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						{localError.email && (
+							<p className="error-message">{localError.email}</p>
+						)}
+					</div>
+					<div className="input-wrapper">
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							id="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						{localError.password && (
+							<p className="error-message">{localError.password}</p>
+						)}
+						{serverError && <p className="error-message">{serverError}</p>}
+					</div>
+					<div className="input-remember">
+						<input type="checkbox" id="remember-me" />
+						<label htmlFor="remember-me">Remember me</label>
+					</div>
+
+					<button type="submit" className="sign-in-button">
+						Sign In
+					</button>
+				</form>
+			</section>
+		</main>
+	);
+};
+
 export default Login;
