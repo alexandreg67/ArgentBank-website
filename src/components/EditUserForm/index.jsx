49c7da2b0fb953cf92599c89f46e0208b7961905
Graff Alexandre <alexgraff67@gmail.com>
2024-08-11 16:31:50 +0200

    Ajout du message de confirmation pour edituserform

--- src/components/EditUserForm/index.jsx
+++ src/components/EditUserForm/index.jsx
@@ -6,11 +6,32 @@ import { updateUserProfile } from "../../features/user/userSlice";
 const EditUserForm = ({ user, onCancel }) => {
 	const dispatch = useDispatch();
 	const [username, setUsername] = useState(user.userName);
+	const [errors, setErrors] = useState({});
+	const [confirmationMessage, setConfirmationMessage] = useState("");
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		dispatch(updateUserProfile({ userName: username }));
-		onCancel();
+
+		let validationErrors = {};
+
+		if (!username.trim()) {
+			validationErrors.username = "Username cannot be empty";
+		}
+
+		setErrors(validationErrors);
+
+		if (Object.keys(validationErrors).length === 0) {
+			try {
+				await dispatch(updateUserProfile({ userName: username })).unwrap();
+				setConfirmationMessage("Username has been successfully updated!");
+				setErrors({});
+			} catch (error) {
+				console.error("Failed to update username:", error);
+				setConfirmationMessage(
+					"An error occurred while updating the username."
+				);
+			}
+		}
 	};
 
 	return (
@@ -25,6 +46,9 @@ const EditUserForm = ({ user, onCancel }) => {
 						value={username}
 						onChange={(e) => setUsername(e.target.value)}
 					/>
+					{errors.username && (
+						<p className="error-message">{errors.username}</p>
+					)}
 				</div>
 				<div className="input-wrapper">
 					<label htmlFor="firstName">First Name :</label>
@@ -42,38 +66,11 @@ const EditUserForm = ({ user, onCancel }) => {
 						Cancel
 					</button>
 				</div>
+				{confirmationMessage && (
+					<p className="confirmation-message">{confirmationMessage}</p>
+				)}
 			</form>
 		</section>
-		// <div className="edit-user-form">
-		// 	<h1>Edit user info</h1>
-		// <form onSubmit={handleSubmit}>
-		// 	<div className="input-wrapper">
-		// 		<label htmlFor="username">User Name :</label>
-		// 		<input
-		// 			type="text"
-		// 			id="username"
-		// 			value={username}
-		// 			onChange={(e) => setUsername(e.target.value)}
-		// 		/>
-		// 	</div>
-		// 	<div className="input-wrapper">
-		// 		<label htmlFor="firstName">First Name :</label>
-		// 		<input type="text" id="firstName" value={user.firstName} disabled />
-		// 	</div>
-		// 	<div className="input-wrapper">
-		// 		<label htmlFor="lastName">Last Name :</label>
-		// 		<input type="text" id="lastName" value={user.lastName} disabled />
-		// 	</div>
-		// 	<div className="button-group">
-		// 		<button type="submit" className="update-button">
-		// 			Save
-		// 		</button>
-		// 		<button type="button" className="cancel-button" onClick={onCancel}>
-		// 			Cancel
-		// 		</button>
-		// 	</div>
-		// </form>
-		// </div>
 	);
 };
 
